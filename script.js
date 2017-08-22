f7361a53577bba67d2a83f32c2c304b6f1540d58
John T Cronin <jt.cronin@outlook.com>
2017-08-22 09:20:44 -0500

    working mondrian

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function(){
+
+var paint = "white";
+var color = document.getElementsByClassName('color');
+var row = document.getElementsByClassName('row');
+
+
+for (var i = 0; i<color.length; i++) {
+	color[i].addEventListener('click', function(){
+		paint = event.target.style.backgroundColor;
+	});
+}
+
+for (var i = 0; i<row.length; i++){
+			row[i].addEventListener('click', function(){
+				
+				event.target.style.backgroundColor = paint;
+			});
+		}
+
+
+
+});
